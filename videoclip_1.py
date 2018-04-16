0ef5ebd89dd3aad2886f95e752c0818a1911842c
lidanhua <38416564+lidanhua@users.noreply.github.com>
2018-04-16 15:08:39 +0800

    Programs for cutting videos.

---
+++ videoclip_1.py
@@ -0,0 +1,58 @@
+﻿import moviepy.editor as mpy
+import multiprocessing
+import sys
+
+
+
+# Function for cutting a video clip.
+
+def cutvideo(a, b):
+     video = mpy.VideoFileClip("%s.mp4" % a).subclip(b[0], b[1])
+     return video.write_videofile("【%s】%s.mp4" % (a, b[2]))
+     
+
+
+# Function for translating the start and end point into number of seconds.
+
+def translate(n):
+     l = n.split(":")
+     return (int(l[0]))*60 + int(l[1])
+
+
+# Function for processing editted texts.
+def processtxt(text):
+     clip = text.split(", ", 2)
+     clip[0], clip[1] = translate(clip[0]), translate(clip[1])
+     clip[2] = clip[2].strip('\n')
+     return clip
+
+def processepisode(all_text):
+     t = all_text
+     i = 0
+     e = t.index('\n', i)
+     d = {}
+     while e > -1:
+          k = t[i].strip('\n')
+          d[k] = t[i+1:e]
+          i = e + 1
+          if '\n' in t[i:]:
+               e = t.index('\n', i)
+          else:
+               e = -1
+     return d
+
+# multiprocessing for faster video processing
+if __name__=="__main__":
+     file_object = open("Cut.txt") # Read the start, end point of and titles for clips.
+     try:
+          all_the_text = file_object.readlines()
+     finally:
+          file_object.close()
+     Text = processepisode(all_the_text)
+     for key in Text:
+          for item in Text[key]:
+               x = processtxt(item)
+               p = multiprocessing.Process(target=cutvideo, args=(key, x))
+               p.start()
+               p.join()
+
---
+++ videoclip_2.py
@@ -0,0 +1,43 @@
+﻿import moviepy.editor as mpy
+import multiprocessing
+import sys
+
+
+
+# Function for cutting a video clip.
+
+def cutvideo(a, b, c):
+     video = mpy.VideoFileClip("第7集.mp4").subclip(a[0], a[1])
+     return video.write_videofile("【第7集片段%s-%s】%s.mp4" % (b, c, a[2]))
+
+
+# Function for translating the start and end point into number of seconds.
+
+def translate(n):
+     l = n.split(":")
+     return (int(l[0]))*60 + int(l[1])
+
+
+# Function for processing editing texts.
+def processtxt(text):
+     clip = text.split(", ", 2)
+     clip[0], clip[1] = translate(clip[0]), translate(clip[1])
+     clip[2] = clip[2].strip('\n')
+     return clip
+
+
+# multiprocessing for faster video processing
+if __name__=="__main__":
+     file_object = open("Cut.txt") # Read the start, end point of and titles for clips.
+     try:
+          all_the_text = file_object.readlines()
+     finally:
+          file_object.close()
+     n = len(all_the_text)
+     for item in all_the_text:
+          m = all_the_text.index(item) + 1
+          x = processtxt(item)
+          p = multiprocessing.Process(target=cutvideo, args=(x, m, n))
+          p.start()
+          p.join()
+
