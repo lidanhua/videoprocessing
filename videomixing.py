636b17e3cc5b26e0aa97c0646840ab63bb5dd14d
lidanhua <38416564+lidanhua@users.noreply.github.com>
2018-04-16 15:06:17 +0800

    Program for mixing videos

---
+++ videomixing.py
@@ -0,0 +1,25 @@
+﻿# This is for mixing videos.
+from moviepy.editor import *
+import multiprocessing
+import sys
+import os 
+import natsort # Librabry for correct order.
+
+# Open and read files in wanted order.
+curDir = os.getcwd()
+clips = natsort.natsorted(os.listdir(curDir))
+
+# Function for clips mixing.
+def mixing(v):
+    V = []
+    for fn in v:
+        if fn.endswith(".mp4"):
+            V.append(VideoFileClip(fn))
+    final_video = concatenate_videoclips(V)
+    return final_video.write_videofile("")
+
+# multiprocessing for faster video processing.
+if __name__=="__main__":
+    p = multiprocessing.Process(target=mixing, args=(clips,))
+    p.start()
+    p.join()
