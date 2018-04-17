9e07023c140d3505c826ee90c70f3ed55f0fc32f
lidanhua <38416564+lidanhua@users.noreply.github.com>
2018-04-17 22:32:04 +0800

    Update and rename videoclip(forCuttingASinglevideo).py to videoclip(forCuttingMultipleVideos).py

--- videoclip(forCuttingMultipleVideos).py
+++ videoclip(forCuttingMultipleVideos).py
@@ -1,31 +1,25 @@
-﻿import moviepy.editor as mpy
+import moviepy.editor as mpy
 import multiprocessing
 import sys
 
-
-
 # Function for cutting a video clip.
-
 def cutvideo(a, b):
      video = mpy.VideoFileClip("%s.mp4" % a).subclip(b[0], b[1])
      return video.write_videofile("【%s】%s.mp4" % (a, b[2]))
-     
 
-
-# Function for translating the start and end point into number of seconds.
-
+# Function for translating the starting and end point into number of seconds.
 def translate(n):
      l = n.split(":")
      return (int(l[0]))*60 + int(l[1])
 
-
-# Function for processing editted texts.
+# Function for processing editted texts, including starting points, end points and titles for videoclips.
 def processtxt(text):
      clip = text.split(", ", 2)
      clip[0], clip[1] = translate(clip[0]), translate(clip[1])
      clip[2] = clip[2].strip('\n')
      return clip
 
+# Get episode number (title).
 def processepisode(all_text):
      t = all_text
      i = 0
